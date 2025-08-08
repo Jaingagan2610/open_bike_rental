f4849c90fbc03f2c9b19311690be6073e31d9f58
gagan jain <jaingagan2610@gmail.com>
2025-08-08 22:58:09 +0530

    complete vite error 3

--- ride-haven-udaipur/src/main.tsx
+++ ride-haven-udaipur/src/main.tsx
@@ -1,11 +1,25 @@
-import React from "react"; // Needed if using classic JSX transform
+// import React from "react"; // Needed if using classic JSX transform
+// import { createRoot } from "react-dom/client";
+// import App from "./App.tsx";
+// import "./index.css";
+
+// createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+import React from "react"; 
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
+const rootEl = document.getElementById("root");
+if (rootEl) {
+  const root = createRoot(rootEl);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
